9118ca664938d94b5d67827046134becfa420825
Noah Schütte <66693138+n0j0games@users.noreply.github.com>
2021-05-27 16:56:31 +0200

    Uploading Homepage

---
+++ cookiedisabler.js
@@ -0,0 +1,23 @@
+/* Removing Cookie banner on load if it has been removed once before */
+function loadCookie(){
+  var alreadyChecked = false;
+  if (localStorage && 'alreadyChecked' in localStorage) {
+    alreadyChecked = localStorage.alreadyChecked;
+    if (alreadyChecked == 'true'){
+      var x = document.getElementById("cookieElem");
+      x.remove();
+    }
+  }
+}
+
+/* removing cookie banner smoothly */
+function disable() {
+  var x = document.getElementById("cookieElem");
+  x.className = "cookies";
+  x.style.opacity = 1;
+  x.classList.toggle('active');
+  x.style.opacity = 0;
+  localStorage.alreadyChecked = true;
+  setTimeout(() => {  x.remove(); }, 2000);
+
+}

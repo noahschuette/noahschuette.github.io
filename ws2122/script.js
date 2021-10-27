e504fde6cc5febf69cb8fc392f53f2ea0c8855ed
Noah Schütte <nschuette@techfak.de>
2021-10-27 22:20:27 +0200

    changing ws2122

--- ws2122/script.js
+++ ws2122/script.js
@@ -1,10 +1,44 @@
 var binurl = "https://api.jsonbin.io/v3/b/615251b39548541c29b9a663";
 var apikey = "$2b$10$xnvjY70FJ/3AkP6TgMxOYOpcTWr/12FGKcvzBP5znTq2JoAW5qJO2";
 
+let jsonpart = `    <!--Task -->
+    <p class="title">TO-DO</p>
+    <div id="tasks" class="tasks">
+      <div class="taskInput">
+        <input id="titleInput" type="text" placeholder="New Task">
+        <input id="dateInput" type="text" placeholder="DD/MM" maxlength="5">
+        <button class="submit" onclick="addTask()"><i class="fas fa-arrow-right"></i></button>
+      </div>
+    </div>
+    <!--Punkte-->
+    <p class="title">Punktetabelle</p>
+    <div class="points" id="points">
+      <!--<div class="scala">
+        <p class="scalaTitle">template</p>
+        <p class="scalaPercentage">50%</p>
+      </div>-->
+    </div>
+    <p class="title">Notizen</p>
+    <!--Notes-->
+    <div class="notes" id="notes">
+      Fehler beim Laden der Notizen!
+    </div>
+  </section>`
+
+function onloaded(){
+    getNumberOfWeek();
+}
+
+function showJSONpart(){
+    let rightpart = document.getElementById('rightPart');
+    rightpart.innerHTML += jsonpart;
+
+    readRequest(true);
+
+    document.getElementById('showJSONpart').remove();
+}
 
 function readRequest(doRefresh){
-
-    getNumberOfWeek();
 
     let storage = localStorage['jsonbin'];
     if (storage != null && !doRefresh){
@@ -271,6 +305,7 @@ function addTask(){
         return;
 
     let date = document.getElementById("dateInput").value;
+    date = date.replace('.','/');
     let regex = /^[0-9]{1,2}\/[0-9]{1,2}$/
     let re = date.match(regex);
     if (re === null)
@@ -308,14 +343,64 @@ function addTask(){
 function setTasks(taskObj){
     let upper = document.getElementById("tasks");
     upper.innerHTML = "";
+    let names = [];
+    let dates = [];
+    let now = Date.now();
 
     for (let k in taskObj){
         let title = getValue(taskObj[k],"title");
-        let date = getValue(taskObj[k],"date");
-        let day = date.split('/')[0];
-        let month = date.split('/')[1];
+        let rawdate = getValue(taskObj[k],"date");
+        let day = rawdate.split('/')[0];
+        let month = rawdate.split('/')[1];
+        let date = new Date();
+        if (month < new Date().getMonth())
+            date.setFullYear(date.getFullYear()+1);
+        date.setMonth(month-1);
+        date.setDate(day);
 
-        let added = `<div id="task${k}" class="task"><button onclick="removeTask(${k})"><i class="fas fa-check"></i></button><p class="taskTitle">${title}</p><p class="taskDate">${day}.${month}.</p></div>`
+        let inserted = false;
+
+        let maintime = Math.ceil(Math.abs(now - date.getTime()) / (1000 * 60 * 60 * 24));
+
+        for (let j in names){
+
+            let temptime = Math.ceil(Math.abs(now - dates[j].getTime()) / (1000 * 60 * 60 * 24));
+            if (maintime > temptime){
+                if (j === 0){
+                    names.push(title);
+                    dates.push(date);
+                } else {
+                    names.splice(j, 0, title);
+                    dates.splice(j, 0, date);
+                }
+                inserted = true;
+                break;
+            }
+        }
+        if (!inserted){
+            names.push(title);
+            dates.push(date);
+        }
+    }
+
+    for (let i= (names.length-1); i >= 0; i--){
+        let title = names[i];
+        let date = dates[i];
+        let day = date.getDate();
+        let month = date.getMonth()+1;
+
+        if (day.length === 1)
+            day = '0' + day;
+        if (month.length === 1)
+            month = '0' + month;
+
+        let datediff = Math.ceil(Math.ceil(now - date.getTime()) / (1000 * 60 * 60 * 24));
+
+        let added = "";
+        if (datediff <= 0)
+            added = `<div id="task${i}" class="task"><button onclick="removeTask(${i})"><i class="fas fa-check"></i></button><p class="taskTitle">${title}</p><p class="taskDate">${day}.${month}.</p></div>`;
+        else
+            added = `<div id="task${i}" class="task outdated"><button onclick="removeTask(${i})"><i class="fas fa-check"></i></button><p class="taskTitle">${title}</p><p class="taskDate outdatedDate">${day}.${month}. - Seit ${datediff} Tagen überfällig</p></div>`;
         upper.innerHTML += added;
     }
 

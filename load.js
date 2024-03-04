dff379b61407634354d1cae4c82e4cf98cc0eefd
Noah <66693138+n0j0games@users.noreply.github.com>
2024-03-04 17:37:10 +0100

    added project pages

--- load.js
+++ load.js
@@ -12,6 +12,8 @@ function load() {
     console.log(isOnMobile);
     
     html = {
+        leftcontent : document.getElementById("leftContent"),
+        rightcontent : document.getElementById("rightContent"),
         spotify : {
             self : document.getElementById("spotify"),
             artist : document.getElementById("spotifyArtist"),
@@ -38,6 +40,10 @@ function load() {
         avatar : document.getElementById("char"),
         aboutme : document.getElementById("aboutme"),
         current : document.getElementById("current"),
+        projectPage : {
+            self : document.getElementById("projectPage"),
+            main : document.getElementById("project"),
+        },
         mid : document.getElementById("midContent"),
         togglemid_enabled : document.getElementById("toggleMidEnabled"),
         togglemid_disabled : document.getElementById("toggleMidDisabled"),
@@ -160,10 +166,136 @@ function activate(name) {
     } 
 }
 
+function loadProject(name) {
+
+    if (window.screen && window.screen.width < 1100) {
+        if (name === "releasr") {
+            window.location.href = "https://releasr.noahschuette.de";
+        } else if (name === "palettecrafter") {
+            window.location.href = "https://palettecrafter.noahschuette.de";
+        } else if (name === "smashteroid") {
+            window.location.href = "https://n0j0games.itch.io/smashteroid";
+        } else {
+            console.error("Unknown project: " + name);
+        }
+        return;
+    }
+
+    const releasr = {
+        title : "RELEASR",
+        desc : "Spotify Release Tracker",
+        tags : ["web", "spotify-api", "js", "html/css"],
+        fullDesc : `Artist release tracker for Spotify. Get notified when your favorite artists release new music.<br><br>
+        Features:<br>
+        - Synchronize your followed artists with Spotify<br>
+        - Alternatively add artists manually<br>
+        - Search for new releases in playlists like "New Music Friday"<br>
+        - Various filters & highly customizable<br>`,
+        links : {
+            website : "https://releasr.noahschuette.de",
+            website_name : "releasr.noahschuette.de",
+            repo : "spotifyreleasetracker",
+        },
+        src : "https://releasr.noahschuette.de/images/favicon.png",
+        status : "active"
+    }
+
+    const palettecrafter = {
+        title : "PALETTECRAFTER",
+        desc : "Minecraft Color-Palette Generator",
+        tags : ["web", "node-js", "html/css", "minecraft"],
+        fullDesc : `Pick any color and this generator finds the matching minecraft block<br><br>
+        Features:<br>
+        - Find matching blocks based on a color<br>
+        - Create palettes from these blocks<br>
+        - Save palettes and share them with others<br>`,
+        links : {
+            website : "https://palettecrafter.noahschuette.de",
+            website_name : "palettecrafter.noahschuette.de",
+            repo : "palettecrafter",
+        },
+        src : "https://palettecrafter.noahschuette.de/images/icon.png",
+        status : "active"
+    }
+
+    const smashteroid = {
+        title : "SMASHTEROID",
+        desc : "2D Asteroid Shooter for Android",
+        tags : ["android", "C#", "unity engine"],
+        fullDesc : `Smashteroid is an endless shooter for Android in which you have to destroy asteroids so that your ship doesn't explode.`,
+        links : {
+            website : "https://n0j0games.itch.io/smashteroid",
+            website_name : "n0j0games.itch.io/smashteroid",
+            repo : null,
+        },
+        src : "https://n0j0games.github.io/n0j0games_archive/images/smashteroidLogo.png",
+        status : "finished"
+    }
+
+    if (name === "releasr") {
+        buildProjectPage(releasr);
+        toggle("projectPage");
+    } else if (name === "palettecrafter") {
+        buildProjectPage(palettecrafter);
+        toggle("projectPage");
+    } else if (name === "smashteroid") {
+        buildProjectPage(smashteroid);
+        toggle("projectPage");
+    } else {
+        console.error("Unknown project: " + name);
+    }
+
+}
+
+function buildProjectPage(content) {
+
+    let tags = "";
+    if (content.status === "active") {
+        tags += `<p class="active">In Development</p>`;
+    } else {
+        tags += `<p class="finished">Finished</p>`;
+    }
+    for (let i = 0; i < content.tags.length; i++) {
+        tags += `<p>${content.tags[i]}</p>`;
+    }
+
+    let repo = "";
+    if (content.links.repo !== null) {
+        repo = `
+        <a class="projectLink" target="_blank" href="https://github.com/n0j0games/${content.links.repo}">
+            <i class="fa-brands fa-github"></i>
+            <p>repo: ${content.links.repo}</p>
+        </a>`;
+    }
+
+    const html_ = `
+    <div id="project_top">
+        <img id="projectImg" src="${content.src}" alt="">
+        <div id="project_right">
+            <p id="projectTitle">${content.title}</p>
+            <p id="projectDesc">${content.desc}</p>
+            <div id="project_tags">
+                ${tags}
+            </div>
+        </div>
+    </div> 
+    <p id="projectFullDesc">${content.fullDesc}</p>
+    <a class="projectLink" target="_blank" href="${content.links.website}">
+        <i class="fa-solid fa-link"></i>
+        <p>${content.links.website_name}</p>
+    </a>
+    ${repo}`;
+
+    html.projectPage.main.innerHTML = html_;
+}
+
 function toggle(type) {
     html.mid.style.transition = "opacity 1s";
     html.aboutme.style.transition = "opacity 1s";
     html.current.style.transition = "opacity 1s";
+    html.leftcontent.style.transition = "opacity 2s";
+    html.rightcontent.style.transition = "opacity 2s";
+    html.projectPage.self.style.transition = "opacity 1s";
     if (type === "about") {
         html.mid.style.opacity = 0;
         html.aboutme.style.opacity = 1;
@@ -174,6 +306,15 @@ function toggle(type) {
         html.current.style.opacity = 1;
         html.mid.style.zIndex = 0;
         html.current.style.zIndex = 1;
+    } else if (type === "projectPage") {
+        html.current.style.opacity = 0;
+        html.aboutme.style.opacity = 0;
+        html.current.style.zIndex = 0;
+        html.aboutme.style.zIndex = 0;
+        html.mid.style.opacity = 0;
+        html.projectPage.self.style.opacity = 1;
+        html.mid.style.zIndex = 0;
+        html.projectPage.self.style.zIndex = 1;
     } else if (type === "close") {
         html.mid.style.opacity = 1;
         html.aboutme.style.opacity = 0;
@@ -184,6 +325,11 @@ function toggle(type) {
         html.current.style.opacity = 0;
         html.mid.style.zIndex = 1;
         html.current.style.zIndex = 0;
+    } else if (type === "closeProject") {
+        html.mid.style.opacity = 1;
+        html.projectPage.self.style.opacity = 0;
+        html.mid.style.zIndex = 1;
+        html.projectPage.self.style.zIndex = 0;
     }
 }
 
--- parallax.js
+++ parallax.js
@@ -26,10 +26,10 @@ function generateParallax() {
 
     const path = "images/space/"
     const image_pool = [
-        {"name" : "tiny", "length" : 4, "data-depth" : 0.01, "zIndex" : -12, "amount" : scale / 2, "scale" : 30},
-        {"name" : "small", "length" : 3, "data-depth" : 0.05, "zIndex" : -11, "amount" : scale / 64, "scale" : 100},
-        {"name" : "large", "length" : 2, "data-depth" : 0.1, "zIndex" : -10, "amount" : 0, "scale" : 100},
-        {"name" : "huge", "length" : 2, "data-depth" : 0.2, "zIndex" : -9, "amount" : 0, "scale" : 100}
+        {"name" : "tiny", "length" : 4, "data-depth" : 0.1, "zIndex" : -12, "amount" : scale / 2, "scale" : 30},
+        {"name" : "small", "length" : 3, "data-depth" : 0.2, "zIndex" : -11, "amount" : scale / 64, "scale" : 30},
+        {"name" : "large", "length" : 5, "data-depth" : 0.4, "zIndex" : -10, "amount" : scale / 200, "scale" : 50},
+        {"name" : "huge", "length" : 6, "data-depth" : 0.8, "zIndex" : -9, "amount" : scale / 500, "scale" : 70}
     ];
 
     let count = 0;
@@ -37,7 +37,6 @@ function generateParallax() {
         for (let i = 0; i < image_pool[k].amount; i++) {
             let img = document.createElement("img");
             img.src = path + image_pool[k].name + Math.floor(Math.random() * image_pool[k].length) + ".png";
-            console.log(k, img.src);
             img.setAttribute("data-depth", image_pool[k]["data-depth"]);
             img.classList.add("paramimg");
             if (k < 2) {
@@ -49,13 +48,18 @@ function generateParallax() {
             } else {
                 img.style.transform = `rotate(${Math.floor(Math.random() * 360)}deg)`
                 if (Math.random() < 0.3) {
-                    img.style.animation = `star-animation-3 ${Math.floor(Math.random() * 100 + 80)}s infinite linear`;
+                    img.style.animation = `star-animation-3 ${Math.floor(Math.random() * 200 + 80)}s infinite linear`;
                 } else if (Math.random() < 0.6) {
-                    img.style.animation = `star-animation-4 ${Math.floor(Math.random() * 100 + 80)}s infinite linear`;
+                    img.style.animation = `star-animation-4 ${Math.floor(Math.random() * 200 + 80)}s infinite linear`;
                 }               
+                img.style.filter = `brightness(${Math.random() * 0.5 + 0.5})`;
             }
             img.style.top = Math.floor(Math.random() * (height - image_pool[k].scale)) + "px";
             img.style.left = Math.floor(Math.random() * (width - image_pool[k].scale)) + "px";
+            if (k == 3) {
+                img.style.width = `${image_pool[k].scale}px`;
+                img.style.height = `${image_pool[k].scale}px`;
+            }
             img.setAttribute("data-top", img.style.top);
             img.setAttribute("data-left", img.style.left);
             scene.appendChild(img);
@@ -71,18 +75,24 @@ function generateParallax() {
 // give random int between 1 and 10
 // Math.floor(Math.random() * 10) + 1
 
-//window.addEventListener('mousemove', mouseMove);
+window.addEventListener('mousemove', mouseMove);
 
 function mouseMove(event) {
-    console.log(event.movementX, event.movementY);
+
+    // get mouse position on screen    
+
+    let mouseX = event.clientX / width * 100 - 50;
+    let mouseY = event.clientY / height * 100 - 50;
+    console.log(mouseX, mouseY);
+
     let scene = document.getElementById('scene');
     for (let i = 0; i < scene.children.length; i++) {
         let img = scene.children[i];
         const top = img.getAttribute("data-top").replace("px", "");
         const left = img.getAttribute("data-left").replace("px", "");
 
-        let new_top = (parseInt(top) + event.movementY * img.getAttribute("data-depth"));
-        let new_left = (parseInt(left) + event.movementX * img.getAttribute("data-depth"));
+        let new_top = (parseInt(top) + mouseY * img.getAttribute("data-depth"));
+        let new_left = (parseInt(left) + mouseX * img.getAttribute("data-depth"));
 
         /*if (new_top < 50) {
             new_top = height - new_top ;
@@ -94,8 +104,8 @@ function mouseMove(event) {
 
         img.style.top = new_top + "px";
         img.style.left = new_left + "px";
-        img.setAttribute("data-top", img.style.top);
-        img.setAttribute("data-left", img.style.left);
+        //img.setAttribute("data-top", img.style.top);
+        //img.setAttribute("data-left", img.style.left);
         //img.style.transform = 'translate(' + event.movementX * img.getAttribute("data-depth") + 'px,' + event.movementY * img.getAttribute("data-depth") + 'px)';
     }
 }
